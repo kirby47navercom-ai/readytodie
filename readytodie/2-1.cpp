d13c9d244df74f37f5a38d66037a455a9382769b
Yang hyeon bin <kirby47naver.com@tukorea.ac.kr>
2025-09-19 16:31:27 +0900

    함수 다시 만듬 좌표 따로 소수점으로 계산안해도 댐

---
+++ readytodie/2-1.cpp
@@ -0,0 +1,258 @@
+#include <gl/glew.h>
+#include <gl/freeglut.h>
+#include <gl/freeglut_ext.h> 
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include <glm/ext.hpp>
+#include "dtd.h" 
+#define SIZE 600
+int x,y;
+float f=0.1f,l=0.1f,o=0.1f;
+int star=0;
+vector<GLfloat> a,c;
+vector<int>_i;
+int time_count=0;
+int stack_count=0;
+int check=0;
+//glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+//glPolygonMode(GL_FRONT_AND_BACK,GL_Line);
+//glRectf(-0.5f,-0.5f,0.5f,0.5f);
+bool left_mouse=false;
+random_device rd;
+mt19937 gen(rd());
+uniform_int_distribution<int> dis(0,1000);
+uniform_int_distribution<int> did(-1000,1000);
+
+struct FPOINT{
+	GLfloat x;
+	GLfloat y;
+};
+struct FRECT{
+	GLfloat left;
+	GLfloat top;
+	GLfloat right;
+	GLfloat bottom;
+};
+FRECT test;
+struct dtd{
+	RECT rect;
+	float r=0,g=0,b=0;
+	int num=0;
+};
+vector<dtd> arr;
+
+GLvoid drawScene(GLvoid);
+GLvoid Reshape(int w,int h);
+void idleScene();
+void Keyboard(unsigned char key,int x,int y);
+void Keyupboard(unsigned char key,int x,int y);
+void SpecialKeyboard (int key,int x,int y);
+void Mouse (int button,int state,int x,int y);
+void Motion (int x,int y);
+int glutGetModifiers ();
+void TimerFunction (int value);
+//void MakeMenu ();
+//void MenuFunction (int button);
+
+GLfloat tranformx(int x){
+	return (((float)x/(SIZE/2))-1.0f)*1000;
+}
+GLfloat tranformy(int y){
+	return (((SIZE - (float)y)/(SIZE/2)) -1.0f)*1000;
+}
+void OffsetFRect(FRECT& rect,GLfloat x,GLfloat y){
+	rect.left+=x;
+	rect.top+=y;
+	rect.right+=x;
+	rect.bottom+=y;
+}
+void InflateFRect(FRECT& rect,GLfloat x,GLfloat y){
+	rect.left-=x;
+	rect.top-=y;
+	rect.right+=x;
+	rect.bottom+=y;
+}
+bool FPtInFRect(const FRECT& rect,const FPOINT& point){
+	return (std::min(rect.left,rect.right) <= point.x && point.x <= std::max(rect.left,rect.right) &&
+			std::min(rect.top,rect.bottom) <= point.y && point.y <= std::max(rect.top,rect.bottom));
+}
+bool IntersectFRect(FRECT &check,const FRECT& rect1,const FRECT& rect2){
+
+	check.left = std::max(rect1.left,rect2.left);
+	check.top = std::max(rect1.top,rect2.top);
+
+
+	check.right = std::min(rect1.right,rect2.right);
+	check.bottom = std::min(rect1.bottom,rect2.bottom);
+
+	if(check.left < check.right && check.top < check.bottom) {
+		return true;
+	} else {
+		check.left = check.top = check.right = check.bottom = 0;
+		return false;
+	}
+}
+void GLRectf(RECT rect){
+	FRECT frect= {static_cast<GLfloat>(rect.left),static_cast<GLfloat>(rect.top),static_cast<GLfloat>(rect.right),static_cast<GLfloat>(rect.bottom)};
+	glRectf(frect.left,frect.top,frect.right,frect.bottom);
+}
+vector<dtd> art;
+
+int main(int argc,char** argv)
+{
+	//--- 윈도우 생성하기		 //--- 윈도우 출력하고 콜백함수 설정
+	glutInit(&argc,argv);		// glut 초기화
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);	// 디스플레이 모드 설정
+	glutInitWindowPosition(100,100);	// 윈도우의 위치 지정
+	glutInitWindowSize(SIZE,SIZE);	// 윈도우의 크기 지정
+	glutCreateWindow("Example1");	// 윈도우 생성 (윈도우 이름)
+	//--- GLEW 초기화하기
+	glewExperimental = GL_TRUE;
+	if(glewInit() != GLEW_OK)	// glew 초기화
+	{
+		std::cerr << "GLEW Initialized\n" << std::endl;
+		exit(EXIT_FAILURE);
+	} else
+		std::cout << "GLEW Initialized\n";
+
+
+	glutTimerFunc (10,TimerFunction,1);
+	glutDisplayFunc(drawScene);	// 출력 함수의 지정
+	glutReshapeFunc(Reshape);	// 다시 그리기 함수 지정
+	glutIdleFunc(idleScene);			// 아이들 타임에 호출하는함수의지정
+	glutKeyboardFunc(Keyboard);			// 키보드 입력 콜백함수
+	glutGetModifiers();				// 컨트롤 알트 시프트 확인
+	glutKeyboardUpFunc(Keyupboard);		// 키보드 뗄때 콜백함수
+	glutSpecialFunc (SpecialKeyboard); // 특수키 입력 콜백함수
+	glutMouseFunc (Mouse);				// 마우스 입력 콜백함수 마우스를 누르고 움직일때
+	glutMotionFunc (Motion);			// 마우스 움직임 콜백함수 마우스를 때고 움직일때
+	glutMainLoop();	// 이벤트 처리 시작
+}
+GLvoid drawScene()
+{
+	glClearColor(f,l,o,1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	// 그리기 부분 구현: 그리기 관련 부분이 여기에포함된다.
+	glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
+	glColor3f (1,1,1);
+	glRectf(0,-1.0f,0,1.0f);
+
+	for(int i=0;i<art.size();++i){
+		GLRectf(art[i].rect);
+	}
+
+	glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
+
+	for(int i=0;i<arr.size();++i){
+		glColor3f (arr[i].r,arr[i].g,arr[i].b);
+		GLRectf(arr[i].rect);
+	}
+	//GLRectf(test);
+	glutSwapBuffers();
+}
+GLvoid Reshape(int w,int h)
+{
+	glViewport(0,0,w,h);
+}
+void idleScene()
+{
+	x += 0.02;
+	y += 0.02;
+	glutPostRedisplay();
+}
+void Keyupboard(unsigned char key,int x,int y){
+
+}
+void Keyboard(unsigned char key,int x,int y)
+{
+	switch(key) {
+	case 'r':
+	{
+		arr.clear();
+		for(int i=0;i<10;++i){
+			
+
+		}
+	}
+	break;
+	case 'q':
+	exit(0);
+	break;
+	}
+}
+void SpecialKeyboard (int key,int x,int y)
+{
+
+}
+void Mouse (int button,int state,int x,int y)
+{
+	if(button == GLUT_LEFT_BUTTON && state == GLUT_DOWN){
+		
+	} else if(button == GLUT_LEFT_BUTTON && state == GLUT_UP){
+		
+	}
+	glutPostRedisplay ();
+
+}
+void Motion (int x,int y)
+{
+	if(left_mouse){
+		
+	}
+
+}
+int glutGetModifiers (){ //컨트롤 알트 시프트 확인
+	return 0;
+
+}
+void TimerFunction (int value)
+{
+	
+	if(!left_mouse){
+		
+	}
+	glutPostRedisplay ();
+	glutTimerFunc (10,TimerFunction,1);
+}
+//void MakeMenu ()
+//{
+//	int SubMenu1,SubMenu2,SubMenu3;
+//	int MainMenu;
+//	SubMenu1 = glutCreateMenu (MenuFunction);
+//	glutAddMenuEntry ("Teapot",1);
+//	glutAddMenuEntry ("Torus",2);
+//	glutAddMenuEntry ("Cone",3);
+//	glutAddMenuEntry ("Cube",4);
+//	glutAddMenuEntry ("Sphere",5);
+//	SubMenu2 = glutCreateMenu (MenuFunction);
+//	glutAddMenuEntry ("Red",11);
+//	glutAddMenuEntry ("Green",22);
+//	SubMenu3= glutCreateMenu (MenuFunction);
+//	glutAddMenuEntry ("Wire",111);
+//	glutAddMenuEntry ("Solid",222);
+//	MainMenu = glutCreateMenu (MenuFunction);
+//	glutAddSubMenu ("Shape",SubMenu1);
+//	glutAddsubMenu ("Color",SubMenu2);
+//	glutAddsubMenu ("State",SubMenu3);
+//	glutAttachMenu (GLUT_RIGHT_BUTTON);
+//	
+//}
+//void MenuFunction (int button)
+//{
+//	//--- button: 메뉴의 구별자값
+//	switch(button) {
+//	case 1: …;
+//		break;
+//	case 2: …;
+//		break;
+//	case 11: …;
+//		break;
+//	case 111:…;
+//	}
+//	//--- Teapot 인 경우
+//   //--- Torus 인 경우
+//   //--- Red 인 경우
+//   //--- Wire인 경우
+//	glutPostRedisplay ();
+//}
